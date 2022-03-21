76d3c2eb2a6e6cd57ec41434f0d82f8a5dd4115a
jsonvas <ysonvas@gmail.com>
2022-03-21 14:08:58 -0500

    update weather-controller

--- controllers/weathers.js
+++ controllers/weathers.js
@@ -3,7 +3,20 @@ const axios = require('axios');
 const get = async (req, res) => {
   const { woeid } = req.params;
   const response = await axios.get(`https://www.metaweather.com/api/location/${woeid}`);
-  res.json(response.data);
+  const forecast = response.data.consolidated_weather.map((item) => {
+    const f = {
+      state_abbr: item.weather_state_abbr,
+      temperature: Math.floor(item.the_temp),
+      state_weather: item.weather_state_name,
+      date: item.applicable_date,
+    }
+    return f
+  })
+  const weather = {
+    title: response.data.title,
+    forecast,
+  };
+  res.json(weather.data);
 }
 
 const searchCountry = async (req, res) => {
