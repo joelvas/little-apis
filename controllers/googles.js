3b355878d4faecc81010320d64546ed289060053
jsonvas <ysonvas@gmail.com>
2022-06-08 11:55:27 -0500

    modified: playwright

--- controllers/googles.js
+++ controllers/googles.js
@@ -3,6 +3,7 @@ const { getImageFromGoogle } = require('../helper/scraping')
 const getImage = async (req, res) => {
   console.time('getImage')
   const link = await getImageFromGoogle(req.query.query)
+  
   console.timeEnd('getImage')
   res.status(200).json(link)
 }
