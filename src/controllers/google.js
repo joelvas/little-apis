756f184647b961e65d77345149d117840a897816
jsonvas <ysonvas@gmail.com>
2022-06-08 23:20:31 -0500

    updated: google controller

--- src/controllers/google.js
+++ src/controllers/google.js
@@ -2,7 +2,7 @@ const { getImageFromGoogle } = require('../helpers/scraping')
 
 const getImage = async (req, res) => {
   console.time('getImage')
-  const link = await getImageFromGoogle(req.params.query)
+  const link = await getImageFromGoogle(req.query.query)
 
   console.timeEnd('getImage')
   res.send('<img src="' + link + '" />')
